a789724bef9c4ca2eb0679983e68e99f5e352c89
William Carriello <william.provision@gmail.com>
2022-05-29 14:47:48 -0300

    Update

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '600px',
+      md: '600px',
+      lg: '600px',
+      xl: '600px',
+      '2xl': '600px',
+    },
+  },
+  corePlugins: {
+    container: false
+  },
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          width: '100%',
+          // marginLeft: 'auto',
+          // marginRight: 'auto',
+          // paddingLeft: '2rem',
+          // paddingRight: '2rem',
+          '@screen sm': {
+            maxWidth: '640px',
+          },
+          '@screen md': {
+            maxWidth: '768px',
+          },
+          '@screen lg': {
+            maxWidth: '1024px',
+          },
+          '@screen xl': {
+            maxWidth: '1280px',
+          },
+        }
+      })
+    }
+  ]
+}
